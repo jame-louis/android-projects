419e4bcdee1e6e4474a38995e59c64c4f595098f
louis <jamelouis@foxmail.com>
2025-12-04 11:09:09 +0800

    fake list view

--- list-view/app/src/main/java/com/example/listview/MainActivity.java
+++ list-view/app/src/main/java/com/example/listview/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<String> cities = CityData.getAllCities();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout city_list = findViewById(R.id.city_list);
+        for(String city : cities) {
+            TextView tv = new TextView(city_list.getContext());
+            tv.setText(city);
+            tv.setTextSize(30);
+            tv.setGravity(Gravity.CENTER);
+            tv.setHeight(128);
+
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(tv.getContext(), tv.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            city_list.addView(tv);
+        }
     }
 }
